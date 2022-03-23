b60761fd565e8e54fd3ffc98b772c22abda55dc0
NyashnyBastard <69503765+NyashnyBastard@users.noreply.github.com>
2022-03-23 19:21:04 +0300

    #Починено

--- app/src/main/java/com/example/test1/MenuActivity.kt
+++ app/src/main/java/com/example/test1/MenuActivity.kt
@@ -1,11 +1,14 @@
 package com.example.test1
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.View
+import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
-import com.example.test1.MenuActivity as MenuActivity1
+
+
 
 class MenuActivity : AppCompatActivity() {
 
@@ -14,27 +17,46 @@ class MenuActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
 
-    }
-    fun callRequest(view: View)
-    {
-        val callRequestIntent = Intent(this, RequestActivity::class.java)
-        startActivity(callRequestIntent)
-    }
-    fun chekList(view: View)
-    {
-        val chekListIntent = Intent(this,cheklistActivity::class.java)
-        startActivity(chekListIntent)
-    }
-    fun Spis(view:View)
-    {
-        val SpisIntent = Intent(this, spisanieActivity::class.java)
-        startActivity(SpisIntent)
-    }
-    fun getOff(view:View){
-        FirebaseAuth.getInstance().signOut()
-        finish()
-    }
+        val mBtn = findViewById<ImageButton>(R.id.messageBtn)
+
+
+        mBtn.setOnClickListener {
+
+            val intent = Intent(Intent.ACTION_SEND)
+            intent.data = Uri.parse("Email")
+            val str_array = arrayOf("")
+            intent.putExtra(Intent.EXTRA_EMAIL, str_array)
+            intent.putExtra(Intent.EXTRA_SUBJECT, "")
+            intent.putExtra(Intent.EXTRA_TEXT, "")
+            intent.type = "message/rfc822"
+            val a = Intent.createChooser(intent, "Перейти в мессенджер")
+            startActivity(a)
+        }
+
+
+        }
 
 
 
-}
+        fun callRequest(view: View) {
+            val callRequestIntent = Intent(this, MenuRequestActivity::class.java)
+            startActivity(callRequestIntent)
+        }
+
+        fun chekList(view: View) {
+            val chekListIntent = Intent(this, cheklistActivity::class.java)
+            startActivity(chekListIntent)
+        }
+
+        fun Spis(view: View) {
+            val SpisIntent = Intent(this, spisanieActivity::class.java)
+            startActivity(SpisIntent)
+        }
+
+        fun getOff(view: View) {
+            FirebaseAuth.getInstance().signOut()
+            finish()
+
+        }
+
+    }
